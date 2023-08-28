45b6432eb11d924470a561be05dfef1936d2e485
itka-school <itka.school@gmail.com>
2023-08-28 11:28:29 +0300

    fixed content

--- src/features/initialStateCreator.js
+++ src/features/initialStateCreator.js
@@ -27,7 +27,6 @@ const slice = createSlice({
           "React / Redux",
           "jQuery",
           "Bootstrap",
-          "Bootstrap",
           "Webpack basics",
           "NodeJS basics",
           "NodeJS basics",
